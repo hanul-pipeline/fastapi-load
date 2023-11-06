f2463ef7c78cb4055b16128c4360dee51cd59cd5
blahxblah <id.blahxblah@gmail.com>
2023-11-06 17:16:22 +0900

    231106 🐰 fix: main.py remove router 수정

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from routers import update_data_router
+from routers import update_parquet_router, remove_data_router
 
 app = FastAPI()
-app.include_router(update_data_router.router)
+app.include_router(update_parquet_router.router)
+app.include_router(remove_data_router.router)
 
 if __name__ == "__main__":
     import uvicorn
